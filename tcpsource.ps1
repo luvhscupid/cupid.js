2cf822b204e93f6ebbe65b979a924395b0b3193e
cupid <49978323+luvhscupid@users.noreply.github.com>
2023-04-03 07:03:35 +0700

    Update tcpsource.ps1

--- tcpsource.ps1
+++ tcpsource.ps1
@@ -1,37 +1,5 @@
-function Gradient($colors, $string) {
-    $rows = 1
-    $cols = $string.Length
-    $step = ($colors.Length - 1) / ($cols - 1)
-
-    for ($i = 0; $i -lt $rows; $i++) {
-        for ($j = 0; $j -lt $cols; $j++) {
-            $t = $j * $step
-            $color1 = $colors[$j % $colors.Length]
-            $color2 = $colors[($j + 1) % $colors.Length]
-            $r = [Math]::Min([Math]::Max([Math]::Round($color1.R + ($color2.R - $color1.R) * $t), 0), 255)
-            $g = [Math]::Min([Math]::Max([Math]::Round($color1.G + ($color2.G - $color1.G) * $t), 0), 255)
-            $b = [Math]::Min([Math]::Max([Math]::Round($color1.B + ($color2.B - $color1.B) * $t), 0), 255)            
-            $rgbColor = [System.Drawing.Color]::FromArgb($r, $g, $b)
-            $consoleColor = $rgbColor.ToKnownColor()
-            Write-Host $string[$j] -NoNewline -ForegroundColor $consoleColor
-        }
-        Write-Host ""
-    }
-}
-$color1 = [System.Drawing.Color]::FromArgb(147, 112, 219)
-$color2 = [System.Drawing.Color]::FromArgb(186, 85, 211)
-$color3 = [System.Drawing.Color]::FromArgb(218, 112, 214)
-$color4 = [System.Drawing.Color]::FromArgb(230, 143, 172)
-$color5 = [System.Drawing.Color]::FromArgb(238, 174, 138)
-$color6 = [System.Drawing.Color]::FromArgb(245, 222, 179)
-
-$gradientColors = @($color1, $color2, $color3, $color4, $color5, $color6)
-
 $welcomeMessage = "Welcome $env:USERNAME! - bing.com @ cupid#0002"
 $lastColor = [System.Drawing.Color]::FromArgb(255, 255, 255)
-$gradientColors += $lastColor
-
-Gradient $gradientColors $welcomeMessage
 
 ## variables ##
 Write-Host "Welcome $env:USERNAME! - bing.com @ cupid#0002" -ForegroundColor Red
@@ -62,7 +30,7 @@ Write-Host "Done!"
 
 ## Menu ##
 while ($true) {
-    Gradient $gradientColors $welcomeMessage
+    Write-Host "Welcome $env:USERNAME! - bing.com @ CupidJS" -ForegroundColor Red
     Write-Host "TCP pingers"
     Write-Host "Graph Ping"
     Write-Host "Quit"
@@ -85,6 +53,6 @@ while ($true) {
 
 ## Run script ##
 Clear-Host
-Gradient $gradientColors $welcomeMessage
+Write-Host "Welcome $env:USERNAME! - bing.com @ CupidJS" -ForegroundColor Red
 & $scriptPath |
 Out-Null
